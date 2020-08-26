561b67f2db7d15b9544a42259cdff322e887b15b
Zachary Chay-Dolan <Zacharia.cd@gmail.com>
2020-08-26 13:14:28 -0400

    pulse is not defined

--- src/sound.js
+++ src/sound.js
@@ -1,10 +1,8 @@
-import { p5 } from "p5";
-
 export default class b2Sound {
   constructor() {}
 
   playPulse() {
-    const p = new p5.Pulse();
+    const p = new Pulse();
     p.amp(0.5);
     p.freq(260);
     p.start();
