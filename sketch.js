aa69e3945dba2b1df95f1283b1e54e8f7b032e43
Franky Martinez <FrankyMartz@gmail.com>
2020-08-26 11:34:52 -0500

    [update]Circle Class, convert to module system

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
+import Circle from './src/Circle.js'
+
+let sphereA;
+
 function setup() {
   createCanvas(900, 600);
+  sphereA = new Circle(createVector(width/2, height/2))
 }
+window.setup = setup;
 
 function draw() {
   background(0);
+  sphereA.run();
 }
+window.draw = draw;
